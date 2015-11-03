dd80cadf1f1016afd3937432f5fc831a8897ad1d
Ben <blinternetgeek@gmail.com>
2015-11-02 22:56:57 -0800

    deleted comments, fixed diet option

--- index.js
+++ index.js
@@ -1,73 +1,7 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var exports = module.exports = {};
-/*
-var options = {
-	diningHall: 0,
-	meal: 1,
-	types: 0
-	callback: callback
-};
-*/
-/*
-exports.getMenu = function(diningHall, meal, callback){
-	var diningHallString;
-	switch(diningHall){
-		case 0: diningHallString = "Bruin%20Plate";
-				break;
-		case 1: diningHallString = "Covel%20Dining";
-				break;
-		case 2: diningHallString = "De%20Neve";
-				break;
-		case 3: diningHallString = "FEAST%20at%20Rieber";
-				break;
-		default: diningHallString = "Bruin%20Plate";
-	}
 
-	var mealString;
-	switch(meal){
-		case 0: mealString = "Breakfast";
-				break;
-		case 1: mealString = "Lunch";
-				break;
-		case 2: mealString = "Dinner";
-				break;
-		default: mealString = "Dinner";
-	}
-
-	var menu = {
-		items: []
-	};
-
-	request('http://m.dining.ucla.edu/menu/index.cfm?m=' + mealString + '&r=' + diningHallString, function (error, response, body) {
-	 if (!error && response.statusCode == 200) {
-
-	    $ = cheerio.load(body);
-	    var numItems = $("div.menu-full.menu-detailed > ol > li").length;
-	    $("div.menu-full.menu-detailed > ol > li > a").each(function(i, element){
-	    	var itemTemplate = {
-		 		name: "",
-		 		isVegan: false,
-		 		isVegetarian: false
-	 		};
-
-	    	var veganText = $(this).find("span").text();
-	    	itemTemplate.isVegan = (veganText == "  vegan");
-	    	itemTemplate.isVegetarian = (veganText == "  vegetarian");
-
-	    	$(this).find("span").remove();
-	    	itemTemplate.name = $(this).text();
-
-	    	menu.items.push(itemTemplate);
-	    });
-
-	    callback(menu);
-	  }
-	})
-}
-
-
-*/
 exports.getMenu = function(options){
 	var diningHallString;
 	switch(options.diningHall){
@@ -101,7 +35,7 @@ exports.getMenu = function(options){
 	 if (!error && response.statusCode == 200) {
 
 	    $ = cheerio.load(body);
-	    var numItems = $("div.menu-full.menu-detailed > ol > li").length;
+	    //var numItems = $("div.menu-full.menu-detailed > ol > li").length;
 	    $("div.menu-full.menu-detailed > ol > li > a").each(function(i, element){
 	    	var itemTemplate = {
 		 		name: "",
@@ -111,7 +45,7 @@ exports.getMenu = function(options){
 
 	    	var veganText = $(this).find("span").text();
 	    	itemTemplate.isVegan = (veganText == "  vegan");
-	    	itemTemplate.isVegetarian = (veganText == "  vegetarian");
+	    	itemTemplate.isVegetarian = (veganText == "  vegetarian") || itemTemplate.isVegan;
 
 	    	$(this).find("span").remove();
 	    	itemTemplate.name = $(this).text();
--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 var library = require('./index.js');
 
-var diningHall = 0, meal = 2;
-
 var options = {
 	diningHall: 0,
 	meal: 2,
-	diet: 0,
+	diet: 1,
 	callback: printMenu
 };
 
-//library.getMenu(diningHall, meal, printMenu);
 library.getMenu(options);
 
 
@@ -17,6 +14,7 @@ library.getMenu(options);
 
 function printMenu(menu){
 	for(var i = 0; i < menu.items.length; i++){
-    	console.log(menu.items[i].name + ", isVegan: " + menu.items[i].isVegan);
+    	//console.log(menu.items[i].name + ", isVegan: " + menu.items[i].isVegan);
+    	console.log(menu.items[i].name + ", isVegetarian: " + menu.items[i].isVegetarian);
     }
 }
